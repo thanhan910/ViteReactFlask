3ed2d4410f9d4ae7eff27dc08d3db95ca8994510
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-08-10 17:04:09 +1000

    Change instruction url for setting up proxy server

--- vite-frontend/vite.config.js
+++ vite-frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/server-options.html#server-proxy
 export default defineConfig({
   plugins: [react()],
   server: {
